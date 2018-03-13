21841c310c59d272900ed3c3b841e7b7bc54fd76
Buuz135 <buuz135@hotmail.com>
2018-03-13 15:50:31 +0100

    Updated version, closes #1

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 1.0.1
++ Fixed a server side crash
--- src/main/java/com/buuz135/smelly/Smelly.java
+++ src/main/java/com/buuz135/smelly/Smelly.java
@@ -27,7 +27,7 @@ public class Smelly {
 
     public static final String MOD_ID = "smelly";
     public static final String MOD_NAME = "Smelly";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     @Mod.Instance(MOD_ID)
     public static Smelly INSTANCE;
